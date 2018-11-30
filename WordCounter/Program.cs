9162c89a3153b696c2595f621cc48267bf35ba04
Student <example@example.com>
2018-11-30 08:20:47 -0800

    set up intial mvc build and restore

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,22 +1,62 @@
-using System;
-
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace WordCountName
 {
-
   public class Program
   {
-    static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("enter a word");
-      string userWord = Console.ReadLine();
-      // var array1 = userWord.ToCharArray();
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-      Console.WriteLine("enter a sentence");
-      string userSentence = Console.ReadLine();
-
-int Count = WordCounter.ReturnCount(userWord,userSentence);
-Console.WriteLine(Count);
-  }
+      host.Run();
+    }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// using System;
+//
+//
+// namespace WordCountName
+// {
+//
+//   public class Program
+//   {
+//     static void Main()
+//     {
+//       Console.WriteLine("enter a word");
+//       string userWord = Console.ReadLine();
+//       // var array1 = userWord.ToCharArray();
+//
+//       Console.WriteLine("enter a sentence");
+//       string userSentence = Console.ReadLine();
+//
+// int Count = WordCounter.ReturnCount(userWord,userSentence);
+// Console.WriteLine(Count);
+//   }
+//   }
+// }
